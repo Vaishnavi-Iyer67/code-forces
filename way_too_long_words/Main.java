3a675d9b758b5be29500a5898343040043d6c990
Vaishnavi Iyer <iyervaishnavi60@gmail.com>
2026-01-22 20:13:32 +0530

    updated

--- way_too_long_words/Main.java
+++ way_too_long_words/Main.java
@@ -1,23 +1,27 @@
 import java.util.Scanner;
+// 71 -A 
+// rating 800 
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        sc.nextLine(); // consume newline
+        sc.nextLine(); // consume leftover newline
 
-        while (n-- > 0) {
+        for (int i = 0; i < n; i++) {
             String s = sc.nextLine();
 
-            if (s.length() > 10) {
-                System.out.println(
-                    s.charAt(0) + "" + (s.length() - 2) + s.charAt(s.length() - 1)
-                );
-            } else {
+            if (s.length() <= 10) {
                 System.out.println(s);
+            } else {
+                System.out.println(
+                    s.charAt(0) +
+                    "" +
+                    (s.length() - 2) +
+                    s.charAt(s.length() - 1)
+                );
             }
         }
-        sc.close();
     }
 }
