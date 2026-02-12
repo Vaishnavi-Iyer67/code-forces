fa67c470bab85c570316055bc1a06c5ee12a7c3b
Vaishnavi Iyer <iyervaishnavi60@gmail.com>
2026-02-12 21:26:49 +0530

    added domino pilling

---
+++ domino_pilling/Main.java
@@ -0,0 +1,20 @@
+// 50 A - domino pilling 
+// rating 800 
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int res=0;
+        res=(m*n )/2;
+        System.out.println(res);
+         
+
+        
+    
+
+
+    }
+
+}
